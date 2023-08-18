cbe4e2504de21bd1076af87d5e480ae016cf690d
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-08-17 21:49:47 -0230

    changes to resume

--- src/Pages/ResumePage.jsx
+++ src/Pages/ResumePage.jsx
@@ -6,12 +6,14 @@ const ResumePage = () => {
   return (
     <>
       <div className="resume-container">
-        <iframe
-          src={resume}
-          frameborder="0"
-          className="resume"
-          title="resume"
-        ></iframe>
+        <object
+          data={resume}
+          width="100%"
+          height="1000
+        "
+          type="application/pdf"
+          aria-label="resume"
+        ></object>
       </div>
     </>
   );
