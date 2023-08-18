ceee508fb789db0d8ba302eb33ac5f1ee47af859
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-08-18 01:02:25 -0230

    resume update check

--- src/Pages/ResumePage.jsx
+++ src/Pages/ResumePage.jsx
@@ -8,8 +8,8 @@ const ResumePage = () => {
   return (
     <>
       <div className="resume-container">
-        {/* <img className="resume-img" src={resume1} alt="first resume page" />
-        <img className="resume-img" src={resume2} alt="first resume page" /> */}
+        <img className="resume-img" src={resume1} alt="first resume page" />
+        <img className="resume-img" src={resume2} alt="first resume page" />
         {/* <iframe
           src={resume}
           frameborder="0"
